3d89472f82aacca7d7f407cbeaed5971ff4ec875
Sid <92081582+s3dhanth@users.noreply.github.com>
2024-10-03 05:26:56 +0530

    Update test_rag.py

--- test_rag.py
+++ test_rag.py
@@ -6,19 +6,13 @@ import spacy
 from fuzzywuzzy import fuzz, process
 from pinecone import Pinecone
 import spacy
-from query_rag import extract_author_ner
-from query_rag import fuzzy_match_author
-from query_rag import retrieve_from_pinecone
-from query_rag import retrive_from_chatbot
-
+from query_rag_spector import retrieve_from_pinecone
+from query_rag_spector import retrive_from_chatbot
 
 nlp = spacy.load("en_core_web_lg")
 stored_meta = pd.read_csv('arxiv_metadata.csv')
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('allenai-specter')
 api_key = 'd7204d21-cb62-4544-b49c-9169b420c0e1'
-all_authors = stored_meta['Author'].str.split(',').tolist()
-flattened_authors = [author.strip() for sublist in all_authors for author in sublist]
-unique_authors = list(set(flattened_authors))
 
 EVAL_PROMPT = """
 Expected Response: {expected_response}
@@ -28,47 +22,50 @@ Actual Response: {actual_response}
 """
 
 
-def test_conclusion_and_future_work():
-    assert query_and_validate(
-        question="What is the primary focus of the proposed MaMA method?",
-        expected_response="he MaMA method focuses on adapting the full CLIP model to mammography by utilizing its multi-view nature, addressing challenges such as labeled data scarcity, high-resolution images with small regions of interest, and data imbalance.")
-def test_conclusion_and_future_work2():
-    assert query_and_validate(
-        question = "What are the significant challenges associated with applying CLIP to mammography?",
-        expected_response = "The challenges include the scarcity of labeled data, the high-resolution images with small regions of interest, and the imbalance in the data, where most mammograms do not contain cancer.")
-def test_conclusion_and_future_work3():
-    assert query_and_validate(
-        question = "How does the MaMA framework address the issue of limited clinical reports in mammography?",
-        expected_response = "MaMA proposes an intuitive method for template-based report construction from tabular data, which helps enable visual-language pre-training despite the lack of corresponding clinical reports.")
-# Example workflow after extracting authors
 
+def test_conclusion_and_future_workr():
+    assert query_and_validate(
+        question = "only give the title of paper where one of the author of paper is Wenliang Zhao or Minglei Shi",
+        expected_response = "FlowTurbo: Towards Real-time Flow-Based Image pyGeneration with Velocity Refiner")
+
+def test_conclusion_and_future_workr1():
+    assert query_and_validate(
+        question = " What technique is used in FlowTurbo to reduce computational costs?",
+        expected_response = "FlowTurbo reduces computational costs by using a lightweight velocity refiner to estimate the velocity during sampling.")
+
+def test_conclusion_and_future_workr2():
+    assert query_and_validate(
+        question = "How does FlowTurbo perform on class-conditional generation in terms of acceleration?",
+        expected_response = "FlowTurbo achieves an acceleration ratio of 53.1%∼58.3% on class-conditional generation")
+
+def test_conclusion_and_future_workr4():
+    assert query_and_validate(
+        question = " How does the velocity predictor behave during the sampling process in flow-based models?",
+        expected_response = "The velocity predictor’s outputs in flow-based models become stable during the sampling process.")
+    
+def test_conclusion_and_future_workr5():
+    assert query_and_validate(
+        question = "How does FlowTurbo improve the inference speed compared to Heun’s method, and what percentage of acceleration is observed?",
+        expected_response = "FlowTurbo improves the inference speed by 37.2% to 43.1% compared to Heun’s method, while still maintaining better sampling quality. It achieves these results by optimizing the sampling configuration through the use of Heun’s blocks, pseudo corrector blocks, and velocity refiner blocks.")
+
+def test_conclusion_and_future_workr6():
+    assert query_and_validate(
+        question = " What is the classifier-free guidance scale (CFG) used during class-conditional image generation, and what is its significance?",
+        expected_response = "The classifier-free guidance scale (CFG) used is 1.5. CFG is significant because it allows the model to generate higher-quality images by adjusting the balance between conditional and unconditional guidance, which enhances the fidelity of generated images")   
+
+def test_conclusion_and_future_workr7():
+    assert query_and_validate(
+        question = "What is the best FID score and the corresponding latency achieved by FlowTurbo in class-conditional image generation experiments?",
+        expected_response = "The best FID score achieved by FlowTurbo in class-conditional image generation experiments is 3.63, with a corresponding latency of 41.6 milliseconds per image, indicating real-time image generation performance.")
+
+def test_conclusion_and_future_workr8():
+    assert query_and_validate(
+        question = "How does the pseudo corrector block affect the sampling speed in the experiments, and what is its contribution to the model’s performance?",
+        expected_response = "The pseudo corrector block significantly improves the sampling speed while introducing a negligible performance drop. For instance, using six pseudo corrector blocks (H1P6R2 configuration) reduced the latency to 41.6 ms/img, achieving a speed-up of 38.7% compared to the baseline while maintaining competitive FID scores.")
 
 def query_and_validate(question: str, expected_response: str):
-    extracted_authors = extract_author_ner(question)
-    if extracted_authors:
-        print(f"Extracted Author from query: {extracted_authors}")
-        
-        # List of authors from vector store metadata (already fetched)
-        # unique_authors is assumed to be defined
-
-        # Perform fuzzy matching to find the closest match
-        matched_authors = fuzzy_match_author(extracted_authors, unique_authors)
-
-        print(f"Matched authors output: {matched_authors}")  # Debugging print statement
-        print(f"Type of matched_authors: {type(matched_authors)}")
-
-        if matched_authors:
-            # Ensure only author names (strings) are extracted
-            matched_author_names = [match[0] for match in matched_authors if isinstance(match[0], str)]
-
-            print(f"Matched authors (only names): (matched_author_names)")
-            results =retrieve_from_pinecone(question, author_name=matched_author_names)
-        else:
-            results =retrieve_from_pinecone(question)
-    else:
-        results = retrieve_from_pinecone(question)
-
-
+    
+    results = retrieve_from_pinecone(question)
     response_text =retrive_from_chatbot(question,results)
         
     prompt = EVAL_PROMPT.format(
